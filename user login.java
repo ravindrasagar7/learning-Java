ae58a6165a273ecb357b0cb108efb99dcb8b4f3d
ravindrasagar7 <41223458+ravindrasagar7@users.noreply.github.com>
2020-02-09 23:01:08 +0530

    Login Program for User

---
+++ user login.java
@@ -0,0 +1,19 @@
+class users {
+private String username;
+private String password;
+Public static void main(String[]Args){
+private String[][] accounts = {{"jesper", "abc123"},{"christian", "abc123"}};
+
+public users(String user, String pass){
+    username = user;
+    password = pass;
+}
+
+public boolean auth(){
+    if((username == accounts[0][0]) && (password == accounts[0][1]))
+        return true;
+    else
+        return false;
+}
+}
+}
